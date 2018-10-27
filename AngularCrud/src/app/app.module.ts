4b9638f616edad181ac39c337c3b4b74454ab897
Avinash <avinashconsultant01@gmail.com>
2018-10-27 06:52:02 -0400

    crud

---
+++ AngularCrud/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { EmployeeService } from './employees/employee.service';
+import { DisplayEmployeeComponent } from './employees/display-employee.component';
+
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent,
+    DisplayEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularCrud/src/app/employees/list-employees.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+// import Employee Model
+import { Employee } from '../models/employee.model';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+
+  employees: Employee[];
+
+
+  // Inject EmployeeService using the constructor
+  // The private variable _employeeService which points to
+  // EmployeeService singelton instance is then available
+  // throughout the class and can be accessed using this keyword
+  constructor(private _employeeService: EmployeeService) { }
+  // Call the getEmployees() service method of EmployeeService
+  // using the private variable _employeeService
+  ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
+  }
+}
